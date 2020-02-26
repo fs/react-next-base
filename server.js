a8df4cfdde5c01b9c3ae75d08e10f7f87f0e7dde
Renas Sitdikov <51324247+RenasSitdikov@users.noreply.github.com>
2020-02-26 11:25:16 +0300

    Server and routes (#1)
    * added server, routes and .env
    
    * added .env configs
    
    * removed .idea
    
    * removed consoles
    
    * added express-force-https

---
+++ server.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const next = require('next');
+const routes = require('./routes');
+const express = require('express');
+const secure = require('express-force-https');
+
+const dev = process.env.NODE_ENV !== 'production';
+// Create the Express-Next App
+const app = next({ dev });
+const handle = routes.getRequestHandler(app);
+// Start the app
+app
+  .prepare()
+  // Start Express server and serve the
+  .then(() => {
+    express()
+      .use(secure)
+      .use(handle)
+      .listen(3000);
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
