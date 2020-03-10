04b3643fc16bf54610f926702ce45f9a0fe17728
Renas Sitdikov <renas.sitdikov@flatstack.com>
2020-03-10 09:40:56 +0300

    added breakpoint functions

--- public/styles/theme.js
+++ public/styles/theme.js
@@ -10,4 +10,10 @@ export default {
     lg: '992px',
     xl: '1200px',
   },
+  up: (breakpoint, vertical = false) => `@media (min-${vertical ? 'height' : 'width'}: calc(${breakpoint} + 0.02px))`,
+  down: (breakpoint, vertical = false) => `@media (max-${vertical ? 'height' : 'width'}: ${breakpoint})`,
+  between: (breakpointMin, breakpointMax, vertical = false) =>
+    `@media (max-${vertical ? 'height' : 'width'}: ${breakpointMax}) and (min-${
+      vertical ? 'height' : 'width'
+    }: calc(${breakpointMin} + 0.02px))`,
 };
