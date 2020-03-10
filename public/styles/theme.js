f66c590cb46c53085fd78a178b4444a9c2149ad3
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2020-03-10 09:18:13 +0300

    change breakpoint values

--- public/styles/theme.js
+++ public/styles/theme.js
@@ -4,12 +4,10 @@ export default {
     white: '#fff',
   },
   breakpoints: {
-    xxs: '320px',
-    xs: '500px',
-    sm: '700px',
+    xs: '320px',
+    sm: '576px',
     md: '768px',
-    lg: '900px',
-    xl: '1100px',
-    xxl: '1300px',
+    lg: '992px',
+    xl: '1200px',
   },
 };
