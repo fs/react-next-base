d5d204fa575694f627577e3a4464a6d41fc1e465
Marat Sadriev <marat.sadriev@flatstack.com>
2020-04-02 11:51:55 +0300

    added static page

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const routes = require('next-routes');
 
 module.exports = routes()
-  .add({ name: 'home', pattern: '/', page: 'index' });
+  .add({ name: 'home', pattern: '/', page: 'index' })
+  .add({ name: 'static-page', pattern: '/static-page', page: 'static-page' });
