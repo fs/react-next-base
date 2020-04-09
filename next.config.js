f24df9e8208819a1c264f70ca9ebfddbc0f53284
IlgizGilmanov <ilgiz.gilmanov@flatstack.com>
2020-04-09 12:06:36 +0300

    next config WIP

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-const dotenv = require('dotenv');
+const Dotenv = require('dotenv-webpack');
 const webpack = require('webpack');
-
-dotenv.config();
+const withPlugins = require('next-compose-plugins');
+const nextCSS = require('@zeit/next-css');
+const nextImages = require('next-images');
+const svgr = require('next-svgr');
 
 const nextConfig = {
   webpack: config => {
@@ -9,12 +11,24 @@ const nextConfig = {
     config.node = {
       fs: 'empty',
     };
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]',
+        },
+      },
+    });
 
     return config;
   },
   env: {
     API_URL: process.env.API_URL,
+    ASSET_HOST: process.env.ASSET_HOST || '',
   },
+  assetPrefix: process.env.ASSET_HOST || '',
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([[nextCSS], [nextImages], [svgr], [new Dotenv()]], nextConfig);
