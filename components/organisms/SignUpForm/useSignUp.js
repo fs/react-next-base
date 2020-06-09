3796a108108b419ad16de7a937786a2a402ccf58
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2020-06-09 13:54:40 +0300

    fix

--- components/organisms/SignUpForm/useSignUp.js
+++ components/organisms/SignUpForm/useSignUp.js
@@ -4,9 +4,10 @@ import SignUp from 'graphql/mutations/signUp.graphql';
 const useSignUp = ({ onSuccess }) => {
   const [mutation, mutationResult] = useMutation(SignUp);
 
-  const signUp = async ({ fistName, lastName, email, password }) => {
+  const signUp = async ({ firstName, lastName, email, password }) => {
+    console.log(firstName, lastName, email, password);
     try {
-      const result = await mutation({ variables: { fistName, lastName, email, password } });
+      const result = await mutation({ variables: { firstName, lastName, email, password } });
       await onSuccess(result);
     } catch (error) {
       console.log(error);
