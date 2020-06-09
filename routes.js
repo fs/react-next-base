df4ef5b0425044e3e26c25f7135016c7ffd6cdb9
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2020-06-09 11:40:03 +0300

    rename login to signup

--- routes.js
+++ routes.js
@@ -3,6 +3,6 @@ const routes = require('next-routes');
 module.exports = routes()
   .add({ name: 'home', pattern: '/', page: 'index' })
   .add({ name: 'formExamples', pattern: '/form-examples', page: 'formExamples' })
-  .add({ name: 'login', pattern: '/login', page: 'login' })
+  .add({ name: 'signUp', pattern: '/signup', page: 'signUp' })
   .add({ name: 'staticPage', pattern: '/static-page', page: 'staticPage' })
   .add({ name: 'pageWithGraphQL', pattern: '/page-with-graphql', page: 'pageWithGraphQL' });
