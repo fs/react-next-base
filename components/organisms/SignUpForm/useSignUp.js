98fdfdfd3c9f634ed2d4f487ba0727c4fd4e0cd6
Nikita Polyakov <nikita.polyakov@flatstack.com>
2020-06-16 11:53:57 +0300

    wip apollo links

--- components/organisms/SignUpForm/useSignUp.js
+++ components/organisms/SignUpForm/useSignUp.js
@@ -1,8 +1,9 @@
 import { useMutation } from '@apollo/react-hooks';
 import SignUp from 'graphql/mutations/signUp.graphql';
+import SignIn from 'graphql/mutations/SignIn.graphql';
 
 const useSignUp = ({ onSuccess }) => {
-  const [mutation, mutationResult] = useMutation(SignUp);
+  const [mutation, mutationResult] = useMutation(SignIn);
 
   const signUp = async (email, password) => {
     try {
