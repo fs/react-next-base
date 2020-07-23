dcf5d3c87df3ee5b57ae61e04066c2d1d7866dba
Nikita-Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2020-07-23 11:42:50 +0300

    User should be able to recover his password (#72)
    * wip recovery
    
    * add useCallback
    
    * useEffect to show message
    
    * remove unused vars
    
    * add try catch for onSubmit

--- components/organisms/LoginForm/LoginForm.js
+++ components/organisms/LoginForm/LoginForm.js
@@ -1,42 +1,101 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import { useSignIn, useSignUp } from 'lib/apollo/hooks/actions';
+import { useSignIn, useSignUp, usePasswordRecovery } from 'lib/apollo/hooks/actions';
 import LoginFormContent from './LoginFormContent';
 
-import { SIGN_IN_FORM, SIGN_UP_FORM } from './constants';
+import { SIGN_IN_FORM, SIGN_UP_FORM, PASSWORD_RECOVERY_FORM } from './constants';
 
-export const StyledFormWrapper = styled.div`
+const StyledFormWrapper = styled.div`
   text-align: center;
+  max-width: 40.625rem;
+  margin: auto;
 `;
 
-export const StyledToggleForm = styled.div`
+const StyledToggleForm = styled.div`
   display: inline-block;
   cursor: pointer;
   color: ${({ theme }) => theme.colors.link};
-  margin-top: 1rem;
+  margin: 1rem 1rem 1rem 0;
 `;
+
+const StyledFormActions = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  margin-bottom: 2rem;
+`;
+
+const StyledMessage = styled.div`
+  color: green;
+  margin-top: 2rem;
+`;
+
+const FORM_ACTIONS = [
+  {
+    to: SIGN_IN_FORM,
+    text: 'Sign In',
+  },
+  {
+    to: SIGN_UP_FORM,
+    text: 'Create an account',
+  },
+  {
+    to: PASSWORD_RECOVERY_FORM,
+    text: 'Forgot your password?',
+  },
+];
 
 const LoginForm = () => {
   const [signIn] = useSignIn();
   const [signUp] = useSignUp();
+  const [recoverPassword, recoverPasswordContext] = usePasswordRecovery();
 
   const [activeForm, setActiveForm] = useState(SIGN_IN_FORM);
+  const [message, setMessage] = useState('');
 
-  const isSignUp = useMemo(() => activeForm === SIGN_UP_FORM, [activeForm]);
+  useEffect(() => {
+    const detail = recoverPasswordContext?.data?.requestPasswordRecovery?.detail;
 
-  const toggleForm = useCallback(() => {
-    setActiveForm(isSignUp ? SIGN_IN_FORM : SIGN_UP_FORM);
-  }, [isSignUp]);
+    if (detail) setMessage(detail);
+  }, [recoverPasswordContext]);
 
-  const onSubmit = useCallback(values => (isSignUp ? signUp(values) : signIn(values)), [isSignUp, signIn, signUp]);
+  const toggleForm = form => {
+    setActiveForm(form);
+    setMessage('');
+  };
 
-  const toggleButtonText = activeForm === SIGN_UP_FORM ? 'Sign in' : 'Create an account';
+  const onSubmit = async (values, { setSubmitting }) => {
+    try {
+      const action = {
+        [SIGN_IN_FORM]: signIn,
+        [SIGN_UP_FORM]: signUp,
+        [PASSWORD_RECOVERY_FORM]: recoverPassword,
+      }[activeForm];
+
+      if (!action) throw new Error(`Action for ${activeForm} form is not exists`);
+
+      setSubmitting(true);
+
+      await action(values);
+
+      setSubmitting(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <StyledFormWrapper>
+      <StyledFormActions>
+        {FORM_ACTIONS.map(({ text, to }) => (
+          <StyledToggleForm key={to} onClick={() => toggleForm(to)}>
+            {text}
+          </StyledToggleForm>
+        ))}
+      </StyledFormActions>
       <LoginFormContent onSubmit={onSubmit} activeForm={activeForm} />
-      <StyledToggleForm onClick={toggleForm}>{toggleButtonText}</StyledToggleForm>
+      <StyledMessage>{message}</StyledMessage>
     </StyledFormWrapper>
   );
 };
