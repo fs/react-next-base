ad0b681b28ec063a7dc4650c1c794900919a2257
Regina-Gimazova <regina.gimazova@flatstack.com>
2020-08-04 10:51:52 +0300

    fix

--- components/atoms/ErrorNotifier/ErrorNotifier.js
+++ components/atoms/ErrorNotifier/ErrorNotifier.js
@@ -3,18 +3,15 @@ import { toast, ToastContainer } from 'react-toastify';
 import useError from 'hooks/useError';
 
 const ErrorNotifier = () => {
-  const { error } = useError();
+  const { error, setError } = useError();
 
+  // Show notifier and clear error before next form submitting
   useEffect(() => {
     if (error) {
-      toast.error(error, {
-        position: 'top-center',
-        autoClose: 5000,
-        hideProgressBar: true,
-        closeOnClick: true,
-      });
+      toast.error(error);
+      setError('');
     }
-  }, [error]);
+  }, [error, setError]);
 
   return <ToastContainer position="top-center" autoClose={5000} closeOnClick hideProgressBar />;
 };
