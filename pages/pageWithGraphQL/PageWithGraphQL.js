2da5ede1da48c001479f09074d7508e2c2727178
Nikita Polyakov <nikita.polyakov@flatstack.com>
2020-08-05 12:18:27 +0300

    add signout button to profile page

--- pages/pageWithGraphQL/PageWithGraphQL.js
+++ pages/pageWithGraphQL/PageWithGraphQL.js
@@ -7,20 +7,28 @@ import ErrorMessage from 'components/atoms/ErrorMessage';
 import WithAuth from 'lib/auth/withAuth';
 import WithAuthSecurity from 'lib/auth/withAuthSecurity';
 import { withApolloClient } from 'lib/withApolloClient';
+import { useSignOut } from 'lib/apollo/hooks/actions';
 
 import DefaultTemplate from 'components/templates/DefaultTemplate';
 
 const PageWithGraphQL = () => {
   const { loading, error, data } = useQuery(CurrentUser);
+  const [signOut] = useSignOut();
 
   const me = data?.me;
   const errorMessage = error ? new ErrorDecorator(error).getMessages() : null;
+  const signOutFromEverywhere = () => signOut({ everywhere: true });
 
   return (
     <DefaultTemplate>
       {loading && <h3>Loading...</h3>}
       {error && <ErrorMessage>{errorMessage}</ErrorMessage>}
-      {!loading && !error && <div>{`This is Current User: ${JSON.stringify(me)}`}</div>}
+      {!loading && !error && (
+        <div>
+          <button onClick={signOutFromEverywhere}>Sign out from all devices</button>
+          {`This is Current User: ${JSON.stringify(me)}`}
+        </div>
+      )}
     </DefaultTemplate>
   );
 };
