bf2d91e79ac52d4ffd23ab1061ff2547fd5a9c95
Marat <marat.sadriev@flatstack.com>
2020-08-11 18:54:04 +0300

    added tests on login and 404 pages

---
+++ cypress/integration/404Page.spec.js
@@ -0,0 +1,15 @@
+describe('404 page testing', () => {
+  it('should show 404-page status', () => {
+    cy.request({
+      url: '/non-existant-page',
+      failOnStatusCode: false,
+    }).then(response => {
+      expect(response.status).to.eq(404);
+    });
+  });
+
+  it('should show 404-page title', () => {
+    cy.visit('/non-existant-page', { failOnStatusCode: false });
+    cy.get('h1').should('contain', "The page you're looking for can't be found.");
+  });
+});
