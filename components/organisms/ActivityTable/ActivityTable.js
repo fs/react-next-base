f058dc10d00edc13de341e7b1c375e1f1241c226
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2020-08-12 12:16:35 +0300

    partial styling

--- components/organisms/ActivityTable/ActivityTable.js
+++ components/organisms/ActivityTable/ActivityTable.js
@@ -1,34 +1,70 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Table = styled.table`
-  width: 100%;
-  padding: 1rem;
+const Table = styled.table(
+  ({ theme: { up, breakpoints } }) =>
+    css`
+      border-spacing: 0;
+      display: block;
+      overflow-x: auto;
+      text-align: left;
+      width: 100%;
+
+      ${up(breakpoints.lg)} {
+        display: table;
+        overflow-x: initial;
+      }
+    `,
+);
+
+const baseCellStyles = css`
+  border-bottom: 1px solid #e1e1e1;
+  padding: 1.2em 1em;
+  color: #606c76;
+  font-size: 0.9em;
+  line-height: 1.4em;
+
+  &:first-child {
+    padding-left: 0;
+  }
+`;
+
+const HeaderCell = styled.th`
+  ${baseCellStyles}
+`;
+const DataCell = styled.td`
+  ${baseCellStyles}
+  font-weight: 100;
+
+  &:nth-child(3) {
+    white-space: nowrap;
+  }
 `;
 
 const ActivityTable = ({ data }) => {
-  console.log('data', data);
+  // console.log('data', data);
   // TODO: show user's avatar before name
+  // TODO: mobile view
   return (
     <Table>
       <thead>
         <tr>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Date</th>
-          <th>User</th>
+          <HeaderCell>Title</HeaderCell>
+          <HeaderCell>Description</HeaderCell>
+          <HeaderCell>Date</HeaderCell>
+          <HeaderCell>User</HeaderCell>
         </tr>
       </thead>
       <tbody>
         {data.map(({ id, title, description, date, name, email }) => {
           return (
             <tr key={id}>
-              <td>{title}</td>
-              <td>{description}</td>
-              <td>{date}</td>
-              <td>
+              <DataCell>{title}</DataCell>
+              <DataCell>{description}</DataCell>
+              <DataCell>{date.toString()}</DataCell>
+              <DataCell>
                 {name} ({email})
-              </td>
+              </DataCell>
             </tr>
           );
         })}
