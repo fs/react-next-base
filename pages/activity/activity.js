262e37acdc51fc87515ce45ffe257b285939fa4a
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2020-08-25 13:01:58 +0300

    Tests

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
 import { css } from 'styled-components';
 
 import WithAuth from 'lib/auth/withAuth';
 import WithAuthSecurity from 'lib/auth/withAuthSecurity';
 import { withApolloClient } from 'lib/withApolloClient';
-import activityEvents from 'config/activityEvents';
-
-import Activities from 'graphql/queries/pages/activities.graphql';
+import { useActivity } from 'lib/apollo/hooks/activity';
 
 import ErrorDecorator from 'decorators/ErrorDecorator';
 import ErrorMessage from 'components/atoms/ErrorMessage';
@@ -18,35 +15,18 @@ const templateStyles = css`
   max-width: 80em;
 `;
 
-const getFormattedActivity = data => {
-  // TODO: implement filter for activity events
-  // TODO: tests
-
-  return data.activities.nodes.map(
-    ({ id, title, body, createdAt, event, user: { firstName, lastName, email, avatarUrl } }) => ({
-      id,
-      title,
-      description: body,
-      date: new Date(createdAt).toLocaleString(),
-      color: activityEvents.find(e => e.name === event).color,
-      name: `${firstName} ${lastName}`,
-      email,
-      avatarUrl,
-    }),
-  );
-};
-
 const Activity = () => {
-  const events = activityEvents.map(({ name }) => name);
-  const { loading, error, data } = useQuery(Activities, { variables: { events } });
+  // TODO: implement filter for activity events
+
+  const { loading, error, activities } = useActivity();
 
   const errorMessage = error ? new ErrorDecorator(error).getMessages() : null;
 
   return (
     <DefaultTemplate contentStyles={templateStyles}>
-      {loading && <h3>Loading...</h3>}
-      {error && <ErrorMessage>{errorMessage}</ErrorMessage>}
-      {!loading && !error && <ActivityTable data={getFormattedActivity(data)} />}
+      {loading && <h3 data-testid="test-activity-loading">Loading...</h3>}
+      {error && <ErrorMessage data-testid="test-activity-error">{errorMessage}</ErrorMessage>}
+      {!loading && !error && <ActivityTable testId="test-activity-table" data={activities} />}
     </DefaultTemplate>
   );
 };
