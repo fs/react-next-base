046aae476c4718c88cc993e7c35ca5678de443c0
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2021-01-21 12:24:18 +0300

    fix snaps, try to use intersept

--- cypress/integration/activities.spec.js
+++ cypress/integration/activities.spec.js
@@ -7,8 +7,11 @@ describe('Activities', () => {
     cy.get('#email').type(EMAIL);
     cy.get('#password').type(PASSWORD);
     cy.get('#signIn').click();
-    cy.get('[data-cy=dropdown-toggler]').click();
-    cy.get('[data-cy=activity]', { timeout: 10000 }).click();
+    cy.intercept('/').as('mainPage');
+    cy.wait('@mainPage');
+
+    cy.get('[data-cy=dropdown-toggler]', { timeout: 1000 }).click();
+    cy.contains('Activity').click();
 
     cy.get('[data-cy=test-activity-table]').should('be.visible');
     //cy.wait('@activityTable');
