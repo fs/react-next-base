04e19ca0c05f0eb355d821ecb9a9101df01c6e59
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2021-02-25 17:42:47 +0300

    fix unit tests

--- components/molecules/ActivityPagination/ActivityPagination.test.js
+++ components/molecules/ActivityPagination/ActivityPagination.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import 'jest-styled-components';
-import '@testing-library/jest-dom/extend-expect';
 
 import renderWithTheme from '__tests__/helpers/renderWithTheme';
 import pageInfoMock from '__tests__/mocks/pageInfoMock';
