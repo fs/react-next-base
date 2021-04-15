2b652d58db39e36936cc96cdb6cf078d361b2efc
Juliana Samoylenko <yuliana.samoilenko@flatstack.com>
2021-04-15 12:02:46 +0300

    change worker

--- worker/index.js
+++ worker/index.js
@@ -1,10 +1,13 @@
-'use strict';
+import 'firebase/messaging';
+import firebase from 'firebase/app';
+import localforage from 'localforage';
+// 'use strict';
 
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
 //
 
-self.__WB_DISABLE_DEV_LOGS = true;
+// self.__WB_DISABLE_DEV_LOGS = true;
 // workbox.setConfig({debug: false})
 
 // const util = require('./util');
@@ -21,40 +24,52 @@ self.__WB_DISABLE_DEV_LOGS = true;
 //   console.log(event.data);
 // });
 
-self.addEventListener('push', function(event) {
-  const data = JSON.parse(event.data.text());
-  event.waitUntil(
-    registration.showNotification(data.title, {
-      body: data.message,
-      icon: 'images/icons/icon-192x192.png',
-    }),
-  );
-});
+const firebaseCloudMessaging = {
+  //checking whether token is available in indexed DB
+  tokenInlocalforage: async () => {
+    return localforage.getItem('fcm_token');
+  },
+  //initializing firebase app
+  init: async function() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyDFh7vqPeYpn910SH57WeofWnMXDc8WJGE',
+        authDomain: 'fir-cloud-messaging-5543f.firebaseapp.com',
+        databaseURL: 'https://fir-cloud-messaging-5543f.firebaseio.com',
+        projectId: 'fir-cloud-messaging-5543f',
+        storageBucket: 'fir-cloud-messaging-5543f.appspot.com',
+        messagingSenderId: '354347614212',
+        appId: '1:354347614212:web:7b474a30c18aa6d2b47551',
+        measurementId: 'G-L63HMWWX6W',
+      });
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
-      if (clientList.length > 0) {
-        let client = clientList[0];
-        for (let i = 0; i < clientList.length; i++) {
-          if (clientList[i].focused) {
-            client = clientList[i];
+      try {
+        const messaging = firebase.messaging();
+        const tokenInLocalForage = await this.tokenInlocalforage();
+        //if FCM token is already there just return the token
+        if (tokenInLocalForage !== null) {
+          return tokenInLocalForage;
+        }
+        //requesting notification permission from browser
+        const status = await Notification.requestPermission();
+        if (status && status === 'granted') {
+          //getting token from FCM
+          const fcm_token = await messaging.getToken({
+            vapidKey: 'BHKTEUH2duqytR3M_MViWykEoTgBb6MtrhkfsrzGkQqVjBJ0ape8ym4zIbx7JluWLfqABa_lykF5cAKcBu_7MGg',
+          });
+          if (fcm_token) {
+            //setting FCM token in indexed db using localforage
+            localforage.setItem('fcm_token', fcm_token);
+            console.log('fcm token', fcm_token);
+            //return the FCM token after saving it
+            return fcm_token;
           }
         }
-        return client.focus();
+      } catch (error) {
+        console.error(error);
+        return null;
       }
-      return clients.openWindow('/');
-    }),
-  );
-});
-
-// self.addEventListener('pushsubscriptionchange', function(event) {
-//   event.waitUntil(
-//       Promise.all([
-//           Promise.resolve(event.oldSubscription ? deleteSubscription(event.oldSubscription) : true),
-//           Promise.resolve(event.newSubscription ? event.newSubscription : subscribePush(registration))
-//               .then(function(sub) { return saveSubscription(sub) })
-//       ])
-//   )
-// })
+    }
+  },
+};
+export { firebaseCloudMessaging };
