94d68ee39b052f9e17ee39c9a91d4944fe09e36c
Marat Sadriev <marat.sadriev@flatstack.com>
2021-04-22 11:47:08 +0300

    pagination button update

---
+++ components/atoms/PaginationButton/PaginationButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import { DefaultTheme } from 'styled-components';
+
+import { StyledButton } from './styled';
+import ITest from 'types/test';
+
+interface Props extends ITest {
+
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  customStyles?: string;
+}
+
+const PaginationButton = ({ testId, disabled, onClick, customStyles, children }: PropsWithChildren<Props>): JSX.Element => {
+  return (
+    <StyledButton
+      type="button"
+      data-cy={testId}
+      data-testid={testId}
+      disabled={disabled}
+      onClick={onClick}
+      customStyles={customStyles}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export default PaginationButton;
