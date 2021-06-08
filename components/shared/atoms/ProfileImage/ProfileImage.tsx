4cfe06a1fb5b948be411cdfacce040cdc9293861
Nadezhda Kharchuk <nadezhda.kharchuk@flatstack.com>
2021-06-08 12:14:42 +0300

    Migrate to ts ProfileImage, DefaultTemplate (#130)

---
+++ components/shared/atoms/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { StyledImageWrapper, StyledImage } from './styled';
+
+interface Props {
+  avatar: string;
+  alt: string;
+}
+
+const ProfileImage = ({ avatar, alt }: Props): JSX.Element => {
+  const defaultAvatar = `${process.env.ASSET_HOST}/images/avatar-placeholder.png`;
+  return (
+    <StyledImageWrapper>
+      <StyledImage alt={alt || 'Avatar'} src={avatar || defaultAvatar} />
+    </StyledImageWrapper>
+  );
+};
+
+export default ProfileImage;
