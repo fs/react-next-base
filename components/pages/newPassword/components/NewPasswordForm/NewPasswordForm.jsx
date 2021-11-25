741ed8d1c5825c17536e77dd70e65d74a58db4d8
Dmitriy Firsov <78723252+DmitriyFirsov@users.noreply.github.com>
2021-11-25 11:17:42 +0300

    move form config out of component

--- components/pages/newPassword/components/NewPasswordForm/NewPasswordForm.jsx
+++ components/pages/newPassword/components/NewPasswordForm/NewPasswordForm.jsx
@@ -7,6 +7,29 @@ import ResetForm from '../ResetForm';
 
 const passwordRegularExp = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])([0-9A-Za-z#$@&!?.*^{}<>;,)(~'"=_%+-]+)$/;
 
+const fields = [
+  {
+    type: 'password',
+    name: 'password',
+    testId: 'password',
+    placeholder: 'Новый пароль',
+    validationSchema: Yup.string()
+      .required('This field is required')
+      .trim()
+      .min(6, 'The minimum password length is 6 characters')
+      .matches(passwordRegularExp, 'Password must contain upper and lower case characters and numbers'),
+  },
+  {
+    type: 'password',
+    name: 'passwordConfirmation',
+    testId: 'passwordConfirmation',
+    placeholder: 'Еще раз новый пароль',
+    validationSchema: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Password does not match')
+      .required('This field is required'),
+  },
+];
+
 const NewPasswordForm = ({ query = {} }) => {
   const [updatePassword] = useUpdatePassword();
 
@@ -18,41 +41,14 @@ const NewPasswordForm = ({ query = {} }) => {
     setSubmitting(false);
   };
 
-  const fields = [
-    {
-      type: 'password',
-      name: 'password',
-      testId: 'password',
-      placeholder: 'Новый пароль',
-    },
-    {
-      type: 'password',
-      name: 'passwordConfirmation',
-      testId: 'passwordConfirmation',
-      placeholder: 'Еще раз новый пароль',
-    },
-  ];
-
   const initialValues = {
     password: '',
     passwordConfirmation: '',
   };
 
-  const validationSchema = Yup.object().shape({
-    password: Yup.string()
-      .required('This field is required')
-      .trim()
-      .min(6, 'The minimum password length is 6 characters')
-      .matches(passwordRegularExp, 'Password must contain upper and lower case characters and numbers'),
-    passwordConfirmation: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Password does not match')
-      .required('This field is required'),
-  });
-
   const form = {
     fields,
     initialValues,
-    validationSchema,
     submit: onSubmit,
   };
 
