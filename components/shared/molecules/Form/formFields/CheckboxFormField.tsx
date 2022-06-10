894fe94b3759a5fdeee242c7294a68aaca0ecf17
Regina-Gimazova <regina.gimazova@flatstack.com>
2022-06-10 17:15:50 +0300

    fix label width

--- components/shared/molecules/Form/formFields/CheckboxFormField.tsx
+++ components/shared/molecules/Form/formFields/CheckboxFormField.tsx
@@ -1,17 +1,22 @@
 import { Field } from 'formik';
 import React from 'react';
+import styled from 'styled-components';
 import { BaseFormFieldConfig, FormFieldType } from 'types/formsType';
 
 export type CheckboxFormFieldConfig = BaseFormFieldConfig & {
   type: FormFieldType.checkbox;
 };
 
+const Label = styled.label`
+  width: max-content;
+`;
+
 const CheckboxFormField = ({ name, label, testID, disabled }: CheckboxFormFieldConfig) => {
   return (
-    <label htmlFor={name}>
+    <Label htmlFor={name}>
       <Field type="checkbox" name={name} id={name} data-testid={testID} data-cy={testID} disabled={disabled} />
       {label}
-    </label>
+    </Label>
   );
 };
 
